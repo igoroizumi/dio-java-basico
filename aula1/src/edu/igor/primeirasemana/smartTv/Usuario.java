2746c20652152197812bd06674396b09eaa77a25
Igor Oizumi Ribeiro <igoroizumi8@gmail.com>
2025-05-15 22:11:03 -0300

    Exemplo de Smart Tv aplicando o que aprendi em metodos

---
+++ aula1/src/edu/igor/primeirasemana/smartTv/Usuario.java
@@ -0,0 +1,39 @@
+package edu.igor.primeirasemana.smartTv;
+
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("A Tv está ligada ? " + smartTv.ligada);
+        System.out.println("Canal Atual " + smartTv.canal);
+        System.out.println("Volume Atual " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -" + "A Tv está ligada ? " + smartTv.ligada);
+        
+        smartTv.desligar();
+        System.out.println("Novo Status -" + "A Tv está ligada ? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        
+
+        smartTv.diminuirVolume();
+
+        smartTv.mudarCanal(17);
+        System.out.println("Canal Atual " + smartTv.canal);
+
+        smartTv.aumentarCanal();
+        System.out.println("Canal Atual " + smartTv.canal);
+
+        smartTv.diminuirCanal();
+        System.out.println("Canal Atual " + smartTv.canal);
+        
+
+
+
+
+
+    }
+    
+}
